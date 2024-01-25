ec07fb58b1eb9d8f04ab9e8ae6c31ee7dcd22aef
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-01-26 02:59:45 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [coinPrice, setCoinPrice] = useState(0);
+  const onChange = (event) => {
+    setMoney(event.target.value);
   };
-  console.log(toDos);
+  const onSelect = (event) => {
+    setCoinPrice(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(coinPrice);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelect}>
+            {coins.map((coin) => (
+              <option value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price.toFixed(2)}
+                USD
+              </option>
+            ))}
+          </select>
+          <input
+            onChange={onChange}
+            type="number"
+            placeholder="Your Deposit"
+          ></input>
+          <span>
+            You can have{" "}
+            {coinPrice !== null ? (money / coinPrice).toFixed(2) : null}
+          </span>
+        </div>
+      )}
     </div>
   );
 }
